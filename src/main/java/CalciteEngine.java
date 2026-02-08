19cf4540f4a9ffd19a8947a0e00ff54ac3a79b20
Pedro <pedrohk@gmail.com>
2026-02-08 03:13:39 -0300

    Calcite

---
+++ src/main/java/CalciteEngine.java
@@ -0,0 +1,34 @@
+import Schema.HrSchema;
+import org.apache.calcite.adapter.java.ReflectiveSchema;
+import org.apache.calcite.jdbc.CalciteConnection;
+import org.apache.calcite.schema.SchemaPlus;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class CalciteEngine {
+    public void runQuery() throws SQLException {
+        Properties info = new Properties();
+        info.setProperty("lex", "JAVA");
+
+        try (Connection connection = DriverManager.getConnection("jdbc:calcite:", info)) {
+            CalciteConnection calciteConnection = connection.unwrap(CalciteConnection.class);
+            SchemaPlus rootSchema = calciteConnection.getRootSchema();
+
+            rootSchema.add("hr", new ReflectiveSchema(new HrSchema()));
+
+            String sql = "SELECT name, email FROM hr.users WHERE id > 1 ORDER BY name DESC";
+            try (Statement statement = calciteConnection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(sql)) {
+
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("name") + " - " + resultSet.getString("email"));
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/Schema/HrSchema.java
@@ -0,0 +1,12 @@
+package Schema;
+
+
+import Model.User;
+
+public class HrSchema {
+    public final User[] users = {
+            new User(1, "Pedro", "pedro@pedro.com"),
+            new User(2, "Pedrinho", "pedrinho@gmail.com"),
+            new User(3, "Pedro Silva", "pedro.silva@brturbo.com.br")
+    };
+}
